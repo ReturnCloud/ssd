fd9230cf843aa97d01c88ac428e8039a12ef7259
ReturnCloud <chinalinshuyun@126.com>
2021-03-10 14:26:25 +0800

    compare with 8803

--- train_cuda0.sh
+++ train_cuda0.sh
@@ -1,1 +1,1 @@
-python train.py --env_name cleanup --cuda 0 --n_rollout_threads 256 --num_mini_batch 1 --num_agents 2 --ppo_epoch 4 --episode_length 50 --lr 2e-3 --value_loss_coef 1 --num_env_steps 70000000
+python train.py --env_name cleanup --cuda 0 --n_rollout_threads 256 --num_mini_batch 1 --num_agents 2 --ppo_epoch 4 --episode_length 50 --lr 2e-3 --value_loss_coef 1 --num_env_steps 70000000 --popart 0
--- train_cuda3.sh
+++ train_cuda3.sh
@@ -1,1 +1,1 @@
-CUDA_DEVICE_VISIBLE=3 python train.py --num_agents 2 --lstm
+python train.py --env_name cleanup --cuda 3 --n_rollout_threads 256 --num_mini_batch 1 --num_agents 2 --ppo_epoch 4 --episode_length 50 --lr 2e-3 --value_loss_coef 1 --num_env_steps 70000000
